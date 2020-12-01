46181d2529ff058e7e53d3d593faca253d2c70f6
Zouhair Loucif <zouhair.loucif@gmail.com>
2020-12-01 03:07:32 +0100

    Updated the site

--- assets/main.js
+++ assets/main.js
@@ -1,34 +1,43 @@
+/**
+ * Variables Declaration
+ *  */
+
+let imageModal = $("#imageModal");
+let videomodal = $("#videoModal");
+let currentvideo = $("#currentvideo");
+let modalImage = $("#img");
+
+/**
+ * Images Part Code
+ *  */
+
 function show_image(obj) {
-    $("#imageModal").modal("show");
+    imageModal.modal("show");
 
     // Get the image and insert it inside the modal - use its "alt" text as a caption
-    $("#img").attr("src", obj.src);
+    modalImage.attr("src", obj.src);
 }
 
 // When the user clicks on <span> (x), close the modal
 $("#close-1").on('click', function () {
-    $("#imageModal").modal("hide");
+    imageModal.modal("hide");
 });
 
-var videomodal = $("#videoModal");
+/**
+ * Videos Part Code
+ *  */
 
 function show_video(id) {
-    $("#videoModal").modal("show");
+    videomodal.modal("show");
 
     // Get the image and insert it inside the modal - use its "alt" text as a caption
-    $("#currentvideo").html('<source src="./assets/videos/vid-0' + id + '.mp4" type="video/mp4" />');
-    $("#currentvideo").get(0).load();
+    currentvideo.html('<source src="./assets/videos/vid-0' + id + '.mp4" type="video/mp4" />');
+    currentvideo.get(0).load();
 }
 
-$("#close-2").on('click', function () {
-    $("#currentvideo").trigger('pause');
-    $("#currentvideo").html('');
-    $("#videoModal").modal("hide");
-});
-
-// Get the <span> element that closes the modal
-
 // When the user clicks on <span> (x), close the modal
-$("#close-2").on("click", function () {
-    $("#videoModal").modal("hide");
+$("#close-2").on('click', function () {
+    currentvideo.trigger('pause');
+    currentvideo.html('');
+    videomodal.modal("hide");
 });
